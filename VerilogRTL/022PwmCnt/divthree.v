a0f5b191cca458717630a8501fbe10eeea8645e4
SunrShine <871457776@qq.com>
2022-06-16 19:48:54 +0800

    Update divthree.v

--- VerilogRTL/022PwmCnt/divthree.v
+++ VerilogRTL/022PwmCnt/divthree.v
@@ -1,7 +1,44 @@
 module divthree(
     input wire clk,
     input wire rst_n,
-    
+    output wire div_three
 );
-    
+    reg [1:0] cnt;
+    reg div_clk1;
+    reg div_clk2;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            cnt <= 0;
+        end
+        else if(cnt == 2)begin
+            cnt <= 0;
+        end
+        else begin
+            cnt <= cnt + 1;
+        end
+    end
+
+    always @(posedge clk or negedge rst_n)begin
+        if(rst_n == 1'b0)begin
+            div_clk1 <= 0;
+        end
+        else if(cnt == 0)begin
+            div_clk1 <= ~div_clk1;
+        end
+        else
+            div_clk1 <= div_clk1;
+    end
+    always @(negedge clk or negedge rst_n)begin
+        if(rst_n == 1'b0)begin
+            div_clk2 <= 0;
+        end
+        else if(cnt == 2)begin
+            div_clk2 <= ~div_clk2;
+        end
+        else
+            div_clk2 <= div_clk2;
+    end
+assign div_three = div_clk2 ^ div_clk1;
+
 endmodule
