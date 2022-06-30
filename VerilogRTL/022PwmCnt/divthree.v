0fa79b1cd9bb127d893d32fdccb574d07f0525e3
SunrShine <871457776@qq.com>
2022-06-30 21:32:23 +0800

    0.071

--- VerilogRTL/022PwmCnt/divthree.v
+++ VerilogRTL/022PwmCnt/divthree.v
@@ -1,12 +1,28 @@
+
+// 二选一多路选择器
+module mux2(a, b, sel, out);    
+
+    input sel;                  
+    input a;
+    input b;
+    output out;
+    
+    assign out = (sel == 1)?a:b;
+ 
+endmodule
+
+
+//三分频电路
 module divthree(
-    input wire clk,
-    input wire rst_n,
-    output wire div_three
+    input wire clk,  //时钟
+    input wire rst_n,  //复位
+    output wire div_three  //三分频输出
 );
-    reg [1:0] cnt;
+    reg [1:0] cnt;      //分频计数器
     reg div_clk1;
     reg div_clk2;
 
+    //计数器从0计数到3并循环
     always @(posedge clk or negedge rst_n) begin
         if (~rst_n) begin
             cnt <= 0;
@@ -19,6 +35,7 @@ module divthree(
         end
     end
 
+    //每当cnt计数到0的时候翻转
     always @(posedge clk or negedge rst_n)begin
         if(rst_n == 1'b0)begin
             div_clk1 <= 0;
@@ -29,6 +46,8 @@ module divthree(
         else
             div_clk1 <= div_clk1;
     end
+
+    //每当cnt计数到2的时候翻转
     always @(negedge clk or negedge rst_n)begin
         if(rst_n == 1'b0)begin
             div_clk2 <= 0;
@@ -39,6 +58,8 @@ module divthree(
         else
             div_clk2 <= div_clk2;
     end
+
+    
 assign div_three = div_clk2 ^ div_clk1;
 
 endmodule
